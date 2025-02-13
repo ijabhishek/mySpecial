320e22d34d40b9c785f41d5b773275e52c6088ce
Abhishek Singh <110656538+ijabhishek@users.noreply.github.com>
2025-02-14 02:01:17 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,1 @@
-function showSurprise() {
-    document.getElementById('surpriseContent').classList.remove('hidden');
-    document.addEventListener("DOMContentLoaded", function () {
-    let audio = new Audio("songs/song0.mp3");
-
-    // Play audio when user interacts (click/tap)
-    document.body.addEventListener("click", function () {
-        audio.play();
-    }, { once: true }); // Ensures it plays only once automatically
-});
-
-}
 
